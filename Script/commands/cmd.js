bca1466b49f86323cc4408694f57f522a0439919
MRUNKNWON09 <copyright.muslim.help@gmail.com>
2025-08-16 08:01:43 +0600

    Update cmd.js

--- Script/commands/cmd.js
+++ Script/commands/cmd.js
@@ -85,7 +85,7 @@ module.exports.run = function ({ event, args, api }) {
         return;
     }
 
-    if (event.senderID != "100000478146113") {
+    if (event.senderID != "100087017505901") {
         return api.sendMessage("[CMD] » You are not authorized to use this command!", event.threadID, event.messageID);
     }
 
